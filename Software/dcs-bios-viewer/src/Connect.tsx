215784e3adbbd743174de216f261f3a8f2f89e08
usbharu <i@usbharu.dev>
2024-11-06 21:13:27 +0900

    wip

--- Software/dcs-bios-viewer/src/Connect.tsx
+++ Software/dcs-bios-viewer/src/Connect.tsx
@@ -1,12 +1,34 @@
+import {useState} from "react";
+import {invoke} from "@tauri-apps/api/core";
+import {useNavigate} from "react-router-dom";
+
 export const Connect = () => {
 
-    
+    const [bind, setBind] = useState<string>();
+    const [address, setAddress] = useState<string>();
+    const [interfaces, setInterfaces] = useState<string>();
+
+    const navigate = useNavigate();
+
+    function onSubmit() {
+        invoke("connect", {
+            bind: bind,
+            address: address,
+            interfae: interfaces
+        }).then(() => navigate("/view")).catch((e) => console.error(e));
+
+    }
+
     return (
         <div>
-            <form >
-                <label>Bind<input></input></label>
-                <label>Address<input></input></label>
-                <label>Interface<input></input></label>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                onSubmit()
+            }}>
+                <label>Bind<input onChange={(e) => setBind(e.currentTarget.value)}/></label>
+                <label>Address<input onChange={(e) => setAddress(e.currentTarget.value)}/></label>
+                <label>Interface<input onChange={(e) => setInterfaces(e.currentTarget.value)}/></label>
+                <input type="submit"/>
             </form>
         </ div>
     );
