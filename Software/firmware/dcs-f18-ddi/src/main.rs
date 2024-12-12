cb3ea4e13b00638078510b3a03b2e380b5dd2198
usbharu <i@usbharu.dev>
2024-12-12 12:44:24 +0900

    マルチプロジェクトな感じに wip

--- Software/firmware/dcs-f18-ddi/src/main.rs
+++ Software/firmware/dcs-f18-ddi/src/main.rs
@@ -14,24 +14,27 @@ use embassy_rp::gpio::Level;
 use embassy_rp::gpio::Output;
 use embassy_rp::i2c::InterruptHandler;
 use embassy_rp::peripherals::*;
+
 use mcp230xx::*;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     I2C1_IRQ => InterruptHandler<I2C1>;
+
 });
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let p: embassy_rp::Peripherals = embassy_rp::init(Default::default());
+    let led: Output<'_> = Output::new(p.PIN_25, Level::Low);
     // I2C
     let i2c_contr = p.I2C1;
     let scl = p.PIN_3;
     let sda = p.PIN_2;
     let hz = 75;
 
-    use embassy_rp::i2c::{self, Config, InterruptHandler};
-    let i2c = i2c::I2c::new_async(i2c_contr, scl, sda, Irqs, Config::default());
+    use embassy_rp::i2c::{self, Config};
+    let i2c: i2c::I2c<'_, I2C1, i2c::Async> =
+        i2c::I2c::new_async(i2c_contr, scl, sda, Irqs, Config::default());
     Mcp230xx::<
         embassy_rp::i2c::I2c<'_, embassy_rp::peripherals::I2C1, embassy_rp::i2c::Async>,
         Mcp23017,
